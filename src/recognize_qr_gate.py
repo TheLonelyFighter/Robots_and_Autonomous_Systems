2ab65053376dacfd140d7091a4f0386bba00524c
Bowen Tan <bowen158@hotmail.com>
2022-03-23 17:41:25 +0200

    Ignore other markers
    Added a quick hack to ignore other markers we don't want to detect, in case they show up in the video frames.

--- src/recognize_qr_gate.py
+++ src/recognize_qr_gate.py
@@ -56,6 +56,7 @@ def main():
     this_aruco_dictionary = cv2.aruco.Dictionary_get(ARUCO_DICT[desired_aruco_dictionary])
         
     frame = cv2.imread('../images/new_markers/qr_codes/frame0025.jpg')
+    marker_ids = [1,2,3,4]
 
     # Detect ArUco markers in the video frame
     # for _, dictionary in enumerate(ARUCO_DICT):
@@ -81,54 +82,55 @@ def main():
     for (marker_corner, marker_id) in zip(corners, ids):
     
         # Extract the marker corners
-        corners = marker_corner.reshape((4, 2))
-        (top_left, top_right, bottom_right, bottom_left) = corners
-        
-        # Convert the (x,y) coordinate pairs to integers
-        top_right = (int(top_right[0]), int(top_right[1]))
-        bottom_right = (int(bottom_right[0]), int(bottom_right[1]))
-        bottom_left = (int(bottom_left[0]), int(bottom_left[1]))
-        top_left = (int(top_left[0]), int(top_left[1]))
+        if(marker_id in marker_ids):
+            corners = marker_corner.reshape((4, 2))
+            (top_left, top_right, bottom_right, bottom_left) = corners
+            
+            # Convert the (x,y) coordinate pairs to integers
+            top_right = (int(top_right[0]), int(top_right[1]))
+            bottom_right = (int(bottom_right[0]), int(bottom_right[1]))
+            bottom_left = (int(bottom_left[0]), int(bottom_left[1]))
+            top_left = (int(top_left[0]), int(top_left[1]))
 
-        # Draw the bounding box of the ArUco detection
-        cv2.line(frame, top_left, top_right, (0, 255, 0), 2)
-        cv2.line(frame, top_right, bottom_right, (0, 255, 0), 2)
-        cv2.line(frame, bottom_right, bottom_left, (0, 255, 0), 2)
-        cv2.line(frame, bottom_left, top_left, (0, 255, 0), 2)
-        
-        # Calculate and draw the center of the ArUco marker
-        center_x = int((top_left[0] + bottom_right[0]) / 2.0)
-        center_y = int((top_left[1] + bottom_right[1]) / 2.0)
-        cv2.circle(frame, (center_x, center_y), 4, (0, 0, 255), -1)
-        
-        # Draw the ArUco marker ID on the video frame
-        # The ID is always located at the top_left of the ArUco marker
-        cv2.putText(frame, str(marker_id), 
-            (top_left[0], top_left[1] - 15),
-            cv2.FONT_HERSHEY_SIMPLEX,
-            0.5, (0, 255, 0), 2)
+            # Draw the bounding box of the ArUco detection
+            cv2.line(frame, top_left, top_right, (0, 255, 0), 2)
+            cv2.line(frame, top_right, bottom_right, (0, 255, 0), 2)
+            cv2.line(frame, bottom_right, bottom_left, (0, 255, 0), 2)
+            cv2.line(frame, bottom_left, top_left, (0, 255, 0), 2)
+            
+            # Calculate and draw the center of the ArUco marker
+            center_x = int((top_left[0] + bottom_right[0]) / 2.0)
+            center_y = int((top_left[1] + bottom_right[1]) / 2.0)
+            cv2.circle(frame, (center_x, center_y), 4, (0, 0, 255), -1)
+            
+            # Draw the ArUco marker ID on the video frame
+            # The ID is always located at the top_left of the ArUco marker
+            cv2.putText(frame, str(marker_id), 
+                (top_left[0], top_left[1] - 15),
+                cv2.FONT_HERSHEY_SIMPLEX,
+                0.5, (0, 255, 0), 2)
 
-        # draw a circle
-        image_size = frame.shape
-        size_x = np.abs(top_right[0] - top_left[0])
-        size_y = np.abs(top_left[1] - bottom_left[1])
+            # draw a circle
+            image_size = frame.shape
+            size_x = np.abs(top_right[0] - top_left[0])
+            size_y = np.abs(top_left[1] - bottom_left[1])
 
 
-        centre_circle_x.append(center_x)
-        centre_circle_y.append(center_y)
+            centre_circle_x.append(center_x)
+            centre_circle_y.append(center_y)
 
-        # if marker_id == 1:  # bottom marker
-        #     centre_circle_x.append(center_x)
-        #     centre_circle_y.append(center_y + radius)
-        # elif marker_id == 2:  # top marker
-        #     centre_circle_x.append(center_x)
-        #     centre_circle_y.append(center_y - radius)
-        # elif marker_id == 3:  # right
-        #     centre_circle_x.append(center_x + radius)
-        #     centre_circle_y.append(center_y)
-        # elif marker_id == 4:  # left
-        #     centre_circle_x.append(center_x - radius)
-        #     centre_circle_y.append(center_y)
+            # if marker_id == 1:  # bottom marker
+            #     centre_circle_x.append(center_x)
+            #     centre_circle_y.append(center_y + radius)
+            # elif marker_id == 2:  # top marker
+            #     centre_circle_x.append(center_x)
+            #     centre_circle_y.append(center_y - radius)
+            # elif marker_id == 3:  # right
+            #     centre_circle_x.append(center_x + radius)
+            #     centre_circle_y.append(center_y)
+            # elif marker_id == 4:  # left
+            #     centre_circle_x.append(center_x - radius)
+            #     centre_circle_y.append(center_y)
 
     centre_circle_x = int(np.round(np.mean(centre_circle_x)))
     centre_circle_y = int(np.round(np.mean(centre_circle_y)))
