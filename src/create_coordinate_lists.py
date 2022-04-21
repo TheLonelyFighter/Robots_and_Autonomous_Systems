771c93c562ff748070f12dc8c286f6b0b4c512f1
MyrtheTi <myrtilleb2@gmail.com>
2022-04-21 11:29:37 +0300

    Creates lists of coordinates for obstacles, goal, and starting point
    Gridmap is removed

---
+++ src/create_coordinate_lists.py
@@ -0,0 +1,65 @@
+# Created by Myrthe Tilleman on 14-4-2022.
+# Description: Creates a grid map and updates the grid map based on obstacles detected.
+
+import numpy as np
+import matplotlib.pyplot as plt
+import cv2
+
+from recognize_obstacle import detect_aruco_code
+
+
+def get_coordinates_line(start_point, end_point, limits, vertical=True):
+    """ Create a list of coordinates of all points from the start point to end point. """
+    x_max = limits[1]
+    y_max = limits[0]
+    if vertical:
+        line = np.arange(np.clip(start_point[1], 0, y_max), np.clip(end_point[1], 0, y_max))
+        coordinate_list = [[np.clip(start_point[0], 0, x_max), y] for y in line]
+    else:  # horizontal line
+        line = np.arange(np.clip(start_point[0], 0, x_max), np.clip(end_point[0], 0, x_max))
+        coordinate_list = [[x, np.clip(start_point[1], 0, y_max)] for x in line]
+    return coordinate_list
+
+
+def get_coordinates(frame):
+    """
+    Get the coordinates of the aruco codes and creates lists of coordinates of
+    the borders of the obstacle, goal area, and the starting position area.
+    """
+    corners, ids, aruco_size = detect_aruco_code(frame)
+    shape = frame.shape[:2]
+
+    obstacle_coordinates = []
+    goal_coordinates = []
+    starting_coordinates = []
+    # loop through detected obstacles
+    for i, (top_left, top_right, bottom_right, bottom_left) in enumerate(corners):
+        # TODO adapt the coordinates to the location of the drone
+        size = aruco_size[i]  # size of the aruco markers in the frame
+        buffer = 2 * size
+
+        top_left_buffer = top_left + [-buffer, -buffer]
+        top_right_buffer = top_right + [buffer, -buffer]
+        bottom_left_buffer = bottom_left + [-buffer, buffer]
+        bottom_right_buffer = bottom_right + [buffer, buffer]
+
+        left_side = get_coordinates_line(top_left_buffer, bottom_left_buffer, shape, vertical=True)
+        right_side = get_coordinates_line(top_right_buffer, bottom_right_buffer, shape, vertical=True)
+        top_side = get_coordinates_line(top_left_buffer, top_right_buffer, shape, vertical=False)
+        bottom_side = get_coordinates_line(bottom_left_buffer, bottom_right_buffer, shape, vertical=False)
+        all_coordinates = left_side + right_side + top_side + bottom_side
+
+        if ids[i] == 1:  # goal
+            goal_coordinates.extend(all_coordinates)
+        elif ids[i] == 4:  # starting point jetbot
+            starting_coordinates.extend(all_coordinates)
+        else:  # obstacle
+            obstacle_coordinates.extend(all_coordinates)
+
+    return obstacle_coordinates, goal_coordinates, starting_coordinates
+
+
+if __name__ == '__main__':
+    image = cv2.imread('../images/new_markers/qr_codes/frame0032.jpg')
+
+    obstacles, goal, start = get_coordinates(image)
